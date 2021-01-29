ac61a3c4a05b93de35fa9a22eaef55242ee74f0d
TheSoundOfTheWind <18270949493@163.com>
2021-01-29 16:31:41 -0600

    add file

---
+++ life/finance/assets_and_liabilities/note/computer/main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main()
+{
+  double con_down;
+  double my_down;
+  double max_down;
+  double all_down;
+  double principal;
+  double investment_amount;
+  do{
+    max_down = con_down + my_down;
+    if (con_decline < -5.0 and max_dwon < -5.0)
+      break;
+    if (my_down < 0.0) {
+      all_down = max_down * 4.0; 
+    } else {
+      all_down = con_down * 2.0;
+    }
+    investment_amount = principal * down;
+  }while(false);
+}
