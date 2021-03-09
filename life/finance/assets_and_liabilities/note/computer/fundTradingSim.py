a9da131ba611156bb0b54671d85d44a2b484117d
TheSoundOfTheWind <18270949493@163.com>
2021-03-09 14:09:01 -0600

    add file

--- life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
+++ life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
@@ -11,17 +11,24 @@ l_t_h   = h_p - l_p
 l_t_c   = c_p - l_p
 c_t_h   = h_p - c_p
 f1  = c_t_h/l_t_h
-f2  = math.pow(f1, f1)
+f2  = math.pow((1+f1), (1+f1))
 print ("down position:", "%.2f%%" % (f1*100.0))
 print ("f1: %.2f" % f1, "f2: %.2f" % f2)
 # down
 c_h_p  = input("current highest point:")
 c_l_p  = input("current lowest point:")
 c_val  = input("valuation:")
-down = (float(c_h_p) - float(c_l_p)) + float(c_val)
+down = (float(c_h_p) - float(c_l_p)) - float(c_val)
+down /= 100.0
+down = down * f2
 principla = 4000.0
 principla = principla*f1
-print ("principal: ", principal)
+print ("principla: %.2f " % principla)
+if (down > 0.05):
+    investment_amount = principla * down
+print ("suggested investment principal: %.2f %%" % (down*100.0))
+print ("suggested single investment amount: %.2f " % investment_amount)
+
 
 
 
