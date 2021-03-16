9d06a776cc38b4d42cc32da8eaa688985e0221c3
TheSoundOfTheWind <18270949493@163.com>
2021-03-16 16:52:37 -0500

    add file

--- life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
+++ life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
@@ -13,40 +13,27 @@ l_p = input("lowest point:")
 l_p = float(l_p)
 c_p = input("current point:")
 c_p = float(c_p)
-last_p = input("last point:")
-last_p = float(last_p)
-last_c_p = input("last current point:")
-last_c_p = float(last_c_p)
 estimate = input("estimate range:")
 estimate = float(estimate)
-
-down = last_c_p - last_p + estimate
-
-if (down < -4.0) :
-    down = -5.0
-elif (down > 4.0):
-    down = 5.0
-
-if (h_p < c_p and down > 0):
-    h_p = c_p + down
+c_p += estimate
+if (h_p < c_p):
+    h_p = c_p
 elif (c_p < l_p):
-    print()
+    l_p = c_p
 l_t_h   = h_p - l_p
 c_t_h   = h_p - c_p
 f1  = c_t_h/l_t_h
 f2  = math.pow((1+f1), (1+f1))
 print ("down position:", "%.2f%%" % (f1*100.0))
 print ("f1: %.2f" % f1, "f2: %.2f" % f2)
-# down
-down /= 100.0
-down = down * f2
+# are the conditions met
+f3 = 5.0 * (1-f1)
+print ("amplitude: %.2f%%" % f3)
+
 principal = 4000.0
-principal = principal*f1
+principal = principal*f1*f2
 print ("principal: %.2f " % principal)
-investment_amount = 0.0
-if (down > 0.05):
-    investment_amount = principal * down
-print ("suggested investment principal: %.2f %%" % (down*100.0))
+investment_amount = principal * 0.05 
 print ("suggested single investment amount: %.2f " % investment_amount)
 
 principal = float(input("principal: "))
