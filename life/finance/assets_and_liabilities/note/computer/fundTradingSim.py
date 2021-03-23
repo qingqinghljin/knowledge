0b73b5b0e812b7772ee5b1ec2c3ebb758a7c8698
TheSoundOfTheWind <18270949493@163.com>
2021-03-23 18:17:55 -0500

    add file

--- life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
+++ life/finance/assets_and_liabilities/note/computer/fundTradingSim.py
@@ -7,13 +7,21 @@ def estimatedRate(cost, pri, c_cost, inves):
     z = 1 - x/y
     return z
 
-
+def fundHightLow(filename, id):
+    f = open(filename, "r")
+    for line in f:
+        a = line.split()
+        b = a[0]
+        if b == id:
+            return a
 
 # h
-h_p = input("highest point:")
-h_p = float(h_p)
-l_p = input("lowest point:")
-l_p = float(l_p)
+id  = input("fund id:")
+list = fundHightLow("../data", id)
+for i in list:
+    print(i)
+l_p = float(list[1])
+h_p = float(list[2])
 c_p = input("current point:")
 c_p = float(c_p)
 estimate = input("estimate range:")
@@ -27,22 +35,23 @@ l_t_h   = h_p - l_p
 c_t_h   = h_p - c_p
 f1  = c_t_h/l_t_h
 f2  = math.pow((1+f1), (1+f1))
+f3  = 0.05*f2
 print ("down position:", "%.2f%%" % (f1*100.0))
-print ("f1: %.2f" % f1, "f2: %.2f" % f2)
+print ("f1: %.2f" % f1, "f2: %.2f" % f2, "f3: %.2f%%" % (f3*100.0))
 # are the conditions met
-f3 = 5.0 * (1-f1)
-print ("amplitude: %.2f%%" % f3)
+f4 = 5.0 * (1-f1)
+print ("amplitude: %.2f%%" % f4)
 
 principal = 4000.0
-principal = principal*f1*f2
+principal = principal*f1
 print ("principal: %.2f " % principal)
-investment_amount = principal * 0.05 
+investment_amount = principal * f3
 print ("suggested single investment amount: %.2f " % investment_amount)
 
-principal = float(input("principal: "))
-position_cost = float(input("position cost: "))
-asset_value = float(input("funds' net asset value: "))
+#principal = float(input("principal: "))
+#position_cost = float(input("position cost: "))
+#asset_value = float(input("funds' net asset value: "))
 
-estimated   = estimatedRate(position_cost, principal, asset_value, investment_amount)
-print("estimated: %.2f " % estimated*100.0)
+#estimated   = estimatedRate(position_cost, principal, asset_value, investment_amount)
+#print("estimated: %.2f " % estimated*100.0)
 
